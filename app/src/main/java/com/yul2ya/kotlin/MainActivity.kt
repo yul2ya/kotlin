cfec25bbc6e9b534dba2f7145eb19229fd2e2f8a
yul2ya <yul2ya@nate.com>
2018-10-29 01:01:18 +0900

    application for chaos team

--- app/src/main/java/com/yul2ya/kotlin/MainActivity.kt
+++ app/src/main/java/com/yul2ya/kotlin/MainActivity.kt
@@ -2,16 +2,66 @@ package com.yul2ya.kotlin
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.*
+import java.util.*
+import kotlin.collections.ArrayList
 
 class MainActivity : AppCompatActivity() {
+    private val checkBoxes = ArrayList<CheckBox>()
+    private var team1 : TextView? = null
+    private var team2 : TextView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        println("Hello world!")
-        var x = max(2, 3)
-        println("max: $x")
+        setTitle("카오스 팀 나누기 made by 율")
+
+        checkBoxes += arrayListOf<CheckBox>(
+                findViewById<CheckBox>(R.id.checkBox1),
+                findViewById<CheckBox>(R.id.checkBox2),
+                findViewById<CheckBox>(R.id.checkBox3),
+                findViewById<CheckBox>(R.id.checkBox4),
+                findViewById<CheckBox>(R.id.checkBox5),
+                findViewById<CheckBox>(R.id.checkBox6))
+
+        team1 = findViewById<TextView>(R.id.team1)
+        team2 = findViewById<TextView>(R.id.team2)
+
+        val button = findViewById<Button>(R.id.button)
+        button.setOnClickListener(View.OnClickListener {
+            val users = ArrayList<String>()
+            for (checkBox in checkBoxes) {
+                if (checkBox.isChecked)
+                    users.add(checkBox.text.toString())
+            }
+            if (users.size <= 1) {
+                Toast.makeText(this, "팀원을 선택하세요.", Toast.LENGTH_SHORT).show()
+                return@OnClickListener
+            }
+            decideTeam(users)
+        })
     }
 
-    fun max(a: Int, b: Int) = if (a > b) a else b
+    fun decideTeam(users : ArrayList<String>) {
+        val heroes = users
+        val random = Random()
+        var team1Str = "팀 1 : "
+        for(i in 0 until heroes.size/2) {
+            var pick = random.nextInt(heroes.size)
+            println(heroes[pick])
+            team1Str += "${heroes[pick]} "
+            heroes.removeAt(pick)
+        }
+        println("vs")
+        var team2Str = "팀 2 : "
+        for(i in heroes) {
+            println(i)
+            team2Str += "$i "
+        }
+
+        team1?.text = team1Str
+        team2?.text = team2Str
+        Toast.makeText(this, "완료되었습니다.", Toast.LENGTH_SHORT).show()
+    }
 }
--- app/src/test/java/com/yul2ya/kotlin/KotlinExample.kt
+++ app/src/test/java/com/yul2ya/kotlin/KotlinExample.kt
@@ -1,6 +1,7 @@
 package com.yul2ya.kotlin
 
 import org.junit.Test
+import java.util.*
 
 class KotlinExample {
 
@@ -27,8 +28,47 @@ class KotlinExample {
         }
     }
 
-    fun test(args: Array<String> = arrayOf() ) {
+    fun test(args: Array<String> = arrayOf()) {
         val name = if (args.size > 0) args[0] else "Kotlin"
         println("Hello, $name!")
+    }
+
+    @Test
+    fun testRangeTo() {
+        val n = 9
+        println(0..n + 1)
+        println((0..(n + 1)).toString())
+        (0..(n + 1)).forEach { print(it) }
+        println()
+        (0 until n + 1).forEach { print(it) }
+    }
+
+    @Test
+    fun testIterator() {
+
+    }
+
+    @Test
+    fun testLambda() {
+        processTheAnswer { number -> number + 1 }
+    }
+
+    fun processTheAnswer(f: (Int) -> Int) {
+        println(f(42))
+    }
+
+    @Test
+    fun decideTeam() {
+        var heroes = arrayListOf<String>("홍재권", "구남도", "김성율", "김문호", "김윤규", "장창우")
+        val random = Random()
+        for(i in 0..2) {
+            var pick = random.nextInt(heroes.size)
+            println(heroes[pick])
+            heroes.removeAt(pick)
+        }
+        println("vs")
+        for(i in heroes) {
+            println(i)
+        }
     }
 }
